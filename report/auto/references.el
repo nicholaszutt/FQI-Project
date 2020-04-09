82aee709a0c0eb5e9bc629ed83444a46410b0db1
Nicholas Zutt <N.C.F.Zutt@student.tudelft.nl>
2020-04-09 13:23:54 +0200

    Adding report template and Swap measurements.

---
+++ report/auto/references.el
@@ -0,0 +1,8 @@
+(TeX-add-style-hook
+ "references"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "nielsen10_quant"
+    "behera19_demon_entan_purif_swapp_protoc"))
+ :bibtex)
+
---
+++ report/auto/report.el
@@ -0,0 +1,44 @@
+(TeX-add-style-hook
+ "report"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "twoside" "twocolumn")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("mathpazo" "sc") ("fontenc" "T1") ("babel" "english") ("geometry" "hmarginratio=1:1" "top=32mm" "columnsep=20pt") ("caption" "format=plain" "small" "labelfont=bf" "up" "textfont=it") ("biblatex" "backend=bibtex" "sorting=none" "isbn=true" "doi=true" "url=false" "eprint=false" "abbreviate=false" "style=numeric")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "abstract"
+    "introduction"
+    "conclusion"
+    "article"
+    "art10"
+    "blindtext"
+    "mathpazo"
+    "fontenc"
+    "microtype"
+    "babel"
+    "graphicx"
+    "amsmath"
+    "amssymb"
+    "geometry"
+    "caption"
+    "booktabs"
+    "lettrine"
+    "units"
+    "enumitem"
+    "titlesec"
+    "fancyhdr"
+    "titling"
+    "hyperref"
+    "siunitx"
+    "biblatex"))
+ :latex)
+
