3e79b159e870a31dac24935cf78d70d9fb060f5c
Nicholas Zutt <N.C.F.Zutt@student.tudelft.nl>
2020-04-10 07:13:11 +0200

    Adding some references and changes.

--- report/auto/references.el
+++ report/auto/references.el
@@ -3,6 +3,8 @@
  (lambda ()
    (LaTeX-add-bibitems
     "nielsen10_quant"
-    "behera19_demon_entan_purif_swapp_protoc"))
+    "behera19_demon_entan_purif_swapp_protoc"
+    "bennett96_purif_noisy_entan_faith_telep"
+    "pan03_exper_entan_purif_arbit_unknow_states"))
  :bibtex)
 
