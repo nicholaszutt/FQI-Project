10f576dc86d5e3891ff1733a58dff441b926024d
Nicholas Zutt <N.C.F.Zutt@student.tudelft.nl>
2020-04-11 14:32:28 +0200

    Update gitignore

--- report/auto/references.el
+++ report/auto/references.el
@@ -5,6 +5,8 @@
     "nielsen10_quant"
     "behera19_demon_entan_purif_swapp_protoc"
     "bennett96_purif_noisy_entan_faith_telep"
-    "pan03_exper_entan_purif_arbit_unknow_states"))
+    "pan03_exper_entan_purif_arbit_unknow_states"
+    "barends14_super_quant_circuit_at_surfac"
+    "kjaergaard19_super_qubit"))
  :bibtex)
 
--- report/auto/report.el
+++ report/auto/report.el
@@ -4,19 +4,21 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "twoside" "twocolumn")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("mathpazo" "sc") ("fontenc" "T1") ("babel" "english") ("geometry" "hmarginratio=1:1" "top=32mm" "columnsep=20pt") ("caption" "format=plain" "small" "labelfont=bf" "up" "textfont=it") ("biblatex" "backend=bibtex" "sorting=none" "isbn=true" "doi=true" "url=false" "eprint=false" "abbreviate=false" "style=numeric")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+                     '(("mathpazo" "sc") ("fontenc" "T1") ("babel" "english") ("geometry" "hmarginratio=1:1" "top=32mm" "columnsep=20pt") ("caption" "format=plain" "small" "labelfont=bf" "up" "textfont=it") ("biblatex" "backend=bibtex" "sorting=none" "isbn=true" "arxiv=false" "doi=false" "url=true" "eprint=false" "abbreviate=false" "style=numeric")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "abstract"
     "introduction"
+    "theory"
+    "results"
     "conclusion"
     "article"
     "art10"
